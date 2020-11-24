684466cba9ceea6d1d42d5099ead703bbffce1e5
imtiiaz <61425608+imtiiaz@users.noreply.github.com>
2020-11-24 20:24:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    // Counter
+
+    $('.count').counterUp({
+        delay: 10,
+        time: 2000
+    });
+    
+    
+     //Mixit UP
+    
+    var mixer = mixitup('.mixitup');
+
+
+});
