0e5e272c88df2df716410340dc11db645292e611
imtiiaz <61425608+imtiiaz@users.noreply.github.com>
2020-11-26 19:54:42 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,11 +6,24 @@ $(function () {
         delay: 10,
         time: 2000
     });
-    
-    
-     //Mixit UP
-    
+
+
+    //Mixit UP
+
     var mixer = mixitup('.mixitup');
 
+    // Progress Bar
+    $('#ios').LineProgressbar({
+        percentage: 95
+    });
+    $('#demoprogressbar2').LineProgressbar({
+        percentage: 74
+    });
+    $('#demoprogressbar3').LineProgressbar({
+        percentage: 65
+    });
+    $('#demoprogressbar4').LineProgressbar({
+        percentage: 25
+    });
 
 });
